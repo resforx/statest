fc96e80b113ca07e4277a0e14921c49a99f0922a
unknown <139689090+resforx@users.noreply.github.com>
2024-04-09 02:12:23 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-import * as fs from 'fs';
+import path from 'path';
+const testResults: { description: string, passed: boolean }[] = [];
+const megaheap = () => path.relative(process.cwd(), __filename);
+
+function be(actual: any, expected: string) {
+    const pass = typeof actual === expected;
+    testResults.push({ description: `Expecting ${typeof actual} to be ${expected}`, passed: pass });
+    if (!pass) { throw new Error(`Expected ${expected} but received ${typeof actual}`) };
+}
+
+function title(title: string, testcase: () => void) {
+    console.log(title);
+    testcase();
+}
+
+function like(liker: string, testcb: () => void) {
+    try {
+        console.log(`   ${liker}`);
+        console.log(`   ✔︎ Passed ${megaheap()}`);
+    } catch (e) {  }
+}
